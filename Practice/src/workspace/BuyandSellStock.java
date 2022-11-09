e991420047d20f954e379b67c0e11d4c6c73db4c
Harisaravanan <hariharan08112001@gmail.com>
2022-11-09 20:44:19 +0530

    Local updates

--- Practice/src/workspace/BuyandSellStock.java
+++ Practice/src/workspace/BuyandSellStock.java
@@ -5,17 +5,16 @@ public class BuyandSellStock {
 
 	public int maxProfit(int[] prices) {
 		int max = 0;
-		
-		for(int i = 0; i <= prices.length - 2; i++) {
-			for(int j = i; j < prices.length; j++) {
-				if(prices[i] < prices[j]) {
-					if(max < (prices[j] - prices[i])) {
-						max = prices[j] - prices[i];
-					}
-				}
+		int buy = 0, sell;
+
+		for(sell = 1; sell < prices.length; ){
+			int res = prices[sell] - prices[buy];
+			max = Math.max(max, res);
+			if(prices[buy] > prices[sell]){
+				buy = sell;
 			}
+			sell++;
 		}
-		
 		return max;
 	}
 	
@@ -23,7 +22,7 @@ public class BuyandSellStock {
 	public static void main(String[] args) {
 		
 		BuyandSellStock obj = new BuyandSellStock();
-		int[] prices = {1,2};
+		int[] prices = {7,2,5,3,6,4,1,9};
 		
 		System.out.println(obj.maxProfit(prices));
 
--- Practice/src/workspace/ThreeSums.java
+++ Practice/src/workspace/ThreeSums.java
@@ -1,5 +1,42 @@
 package workspace;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ThreeSums {
 
+    public ArrayList<Integer> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int currentPos, leftPt;
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for(currentPos = 0; currentPos < nums.length; currentPos++){
+            int rightPt = nums.length - 1;
+            for(leftPt = currentPos + 1; leftPt < rightPt; ){
+                int sum = nums[currentPos] + nums[leftPt] + nums[rightPt];
+
+                if(sum == 0){
+                    result.add(nums[currentPos]);
+                    result.add(nums[leftPt]);
+                    result.add(nums[rightPt]);
+                    break;
+                } else if (sum > 0) {
+                    rightPt--;
+                } else {
+                    leftPt++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ThreeSums obj = new ThreeSums();
+        int[] nums = {-1,0,1,2,-1,-4};
+
+        System.out.println(obj.threeSum(nums));
+    }
 }
